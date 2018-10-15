4f4c7f046a487dc42ddc04b2728a5f808bf58ad0
Nastya <brodskyi@inbox.ru>
2018-10-15 13:09:18 +0300

    For Github Pages

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import React from "react"; 
-import ReactDOM from "react-dom";
-import App from "./app.js";
-
-import "./styles/style.scss";
-
-ReactDOM.render(<App/>, document.getElementById("root"));
-
-module.hot.accept();
